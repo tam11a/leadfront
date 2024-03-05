dbce1e0e6b170ac465ad7da50cadea7a47d7c981
tam11a <ibrahimsadiktamim@gmail.com>
2024-03-06 03:31:39 +0600

    added: dashboard

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -25,23 +25,27 @@ export function CalendarDateRangePicker({
 					<Button
 						id="date"
 						variant={"outline"}
+						// size={"icon"}
 						className={cn(
-							"w-[260px] justify-start text-left font-normal",
+							"justify-center text-left font-normal",
 							!date && "text-muted-foreground"
 						)}
 					>
-						<CalendarIcon className="mr-2 h-4 w-4" />
+						<CalendarIcon className="mr-0 md:mr-2 h-4 w-4" />
 						{date?.from ? (
 							date.to ? (
-								<>
+								<span className="hidden md:inline">
+									{" "}
 									{format(date.from, "LLL dd, y")} -{" "}
 									{format(date.to, "LLL dd, y")}
-								</>
+								</span>
 							) : (
-								format(date.from, "LLL dd, y")
+								<span className="hidden md:inline">
+									{format(date.from, "LLL dd, y")}
+								</span>
 							)
 						) : (
-							<span>Pick a date</span>
+							<span className="hidden md:inline">Pick a date</span>
 						)}
 					</Button>
 				</PopoverTrigger>
