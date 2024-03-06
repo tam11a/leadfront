9c2318d4d6f0a6d8b75c3bd6242d0b0c336638c4
tam11a <ibrahimsadiktamim@gmail.com>
2024-03-06 13:59:05 +0600

    updated: responsive issue temporarily of home page

--- src/app/(private)/dashboard/recent-sales.tsx
+++ src/app/(private)/dashboard/recent-sales.tsx
@@ -13,9 +13,7 @@ export function RecentSales() {
 				</Avatar>
 				<div className="ml-4 space-y-1">
 					<p className="text-sm font-medium leading-none">Olivia Martin</p>
-					<p className="text-sm text-muted-foreground">
-						olivia.martin@email.com
-					</p>
+					<p className="text-sm text-muted-foreground">olivia@email.com</p>
 				</div>
 				<div className="ml-auto font-medium">+$1,999.00</div>
 			</div>
@@ -29,7 +27,7 @@ export function RecentSales() {
 				</Avatar>
 				<div className="ml-4 space-y-1">
 					<p className="text-sm font-medium leading-none">Jackson Lee</p>
-					<p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+					<p className="text-sm text-muted-foreground">jackson@email.com</p>
 				</div>
 				<div className="ml-auto font-medium">+$39.00</div>
 			</div>
@@ -43,9 +41,7 @@ export function RecentSales() {
 				</Avatar>
 				<div className="ml-4 space-y-1">
 					<p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-					<p className="text-sm text-muted-foreground">
-						isabella.nguyen@email.com
-					</p>
+					<p className="text-sm text-muted-foreground">isabella@email.com</p>
 				</div>
 				<div className="ml-auto font-medium">+$299.00</div>
 			</div>
@@ -73,7 +69,7 @@ export function RecentSales() {
 				</Avatar>
 				<div className="ml-4 space-y-1">
 					<p className="text-sm font-medium leading-none">Sofia Davis</p>
-					<p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+					<p className="text-sm text-muted-foreground">sofia@email.com</p>
 				</div>
 				<div className="ml-auto font-medium">+$39.00</div>
 			</div>
