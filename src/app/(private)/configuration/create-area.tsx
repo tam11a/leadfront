2797919462167f48bd1b771cb0ea4547ecc3a0c6
tam11a <ibrahimsadiktamim@gmail.com>
2024-03-10 01:43:23 +0600

    create sheet validation

--- src/app/(private)/configuration/create-area.tsx
+++ src/app/(private)/configuration/create-area.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
 import {
 	Sheet,
 	SheetClose,
@@ -12,15 +19,45 @@ import {
 	SheetFooter,
 	SheetHeader,
 	SheetTitle,
-	SheetTrigger,
 } from "@/components/ui/sheet";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const CreateAreaSchema = z.object({
+	area_name: z
+		.string()
+		.min(1, {
+			message: "Area name must be at least 1 character.",
+		})
+		.max(30, {
+			message: "Area name must not be longer than 30 characters.",
+		}),
+});
+type CreateAreaValues = z.infer<typeof CreateAreaSchema>;
 
 export function CreateSheet() {
+	const [open, setOpen] = useState(false);
+
+	const form = useForm<CreateAreaValues>({
+		resolver: zodResolver(CreateAreaSchema),
+		defaultValues: {
+			area_name: "",
+		},
+		mode: "onChange",
+	});
+
+	async function onSubmit(data: CreateAreaValues) {
+		console.log(data);
+	}
+
 	return (
-		<Sheet>
-			<SheetTrigger asChild>
-				<Button>Add New</Button>
-			</SheetTrigger>
+		<Sheet
+			open={open}
+			onOpenChange={(o) => setOpen(o)}
+		>
+			<Button onClick={() => setOpen(true)}>Add New</Button>
 			<SheetContent>
 				<SheetHeader>
 					<SheetTitle>Create Business Area</SheetTitle>
@@ -28,24 +65,36 @@ export function CreateSheet() {
 						Complete the form below to create a new business area.
 					</SheetDescription>
 				</SheetHeader>
-				<Separator className="mb-3 mt-5" />
-
-				<Label
-					htmlFor="name"
-					className="text-right"
-				>
-					Area Name
-				</Label>
-				<Input
-					id="name"
-					placeholder="Dhaka, Chittagong, etc."
-					className="my-3"
-				/>
-				<SheetFooter>
-					<SheetClose asChild>
-						<Button type="submit">Save</Button>
-					</SheetClose>
-				</SheetFooter>
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-6"
+					>
+						<FormField
+							control={form.control}
+							name="area_name"
+							render={({ field }) => (
+								<FormItem className="mt-5">
+									<FormLabel>Area Name</FormLabel>
+									<FormControl>
+										<Input
+											placeholder="Dhaka, Chittagong, etc."
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription></FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<SheetFooter>
+							<SheetClose asChild>
+								<Button variant={"ghost"}>Cancel</Button>
+							</SheetClose>
+							<Button type="submit">Save</Button>
+						</SheetFooter>
+					</form>
+				</Form>
 			</SheetContent>
 		</Sheet>
 	);
