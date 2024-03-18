c44c49cd8cfc22af7a0019924163b378ac69b3d0
tam11a <ibrahimsadiktamim@gmail.com>
2024-03-19 00:49:05 +0600

    updated: date-picker submission format

--- src/app/(private)/employees/create-employee.tsx
+++ src/app/(private)/employees/create-employee.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
 	Form,
 	FormControl,
@@ -11,6 +12,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
 import {
 	Select,
 	SelectContent,
@@ -31,7 +37,10 @@ import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
 import { useCreateEmployees } from "@/lib/actions/employees/post-employees";
 import handleResponse from "@/lib/handle-response";
+import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -54,7 +63,7 @@ const CreateEmployeeSchema = z.object({
 	phone: z.string().min(11, {
 		message: "Phone number must be at least 11 characters.",
 	}),
-	dob: z.date().optional(),
+	dob: z.any().optional(),
 	work_hour: z.any().optional(),
 	salary: z.any().optional(),
 	bank_name: z.string().optional(),
@@ -266,6 +275,48 @@ export function CreateEmployee() {
 												placeholder="017XXXXXXXX"
 												{...field}
 											/>
+										</FormControl>
+										<FormDescription></FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name="dob"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Date of Birth</FormLabel>
+										<FormControl>
+											<Popover>
+												<PopoverTrigger asChild>
+													<Button
+														variant={"outline"}
+														className={cn(
+															"w-full justify-start text-left font-normal",
+															!field.value && "text-muted-foreground"
+														)}
+													>
+														<CalendarIcon className="mr-2 h-4 w-4" />
+														{field.value ? (
+															format(field.value, "PPP")
+														) : (
+															<span>Pick a date</span>
+														)}
+													</Button>
+												</PopoverTrigger>
+												<PopoverContent className="w-auto p-0">
+													<Calendar
+														mode="single"
+														selected={field.value}
+														onSelect={(e: any) =>
+															field.onChange(format(e as Date, "yyyy-MM-dd"))
+														}
+														initialFocus
+													/>
+												</PopoverContent>
+											</Popover>
 										</FormControl>
 										<FormDescription></FormDescription>
 										<FormMessage />
