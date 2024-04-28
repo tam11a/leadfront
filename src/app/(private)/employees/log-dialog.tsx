11a1b2a74e3a0e63f7b9eac5bb0dafd27836535c
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-28 08:16:59 +0600

    added: log dialog

--- src/app/(private)/employees/log-dialog.tsx
+++ src/app/(private)/employees/log-dialog.tsx
@@ -12,7 +12,9 @@ import {
 } from "@/components/ui/dialog";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useGetLog } from "@/lib/actions/auth/get-log";
+import moment from "moment";
 import { useState } from "react";
 import { FiActivity } from "react-icons/fi";
 
@@ -24,7 +26,9 @@ export function LogDialog({
 	employeeId: number;
 }>) {
 	const [open, setOpen] = useState(false);
-	const { data } = useGetLog(open ? { user: employeeId } : undefined);
+	const { data, isLoading } = useGetLog(
+		open ? { user: employeeId } : undefined
+	);
 	return (
 		<Dialog
 			open={open}
@@ -41,24 +45,47 @@ export function LogDialog({
 					</DialogDescription>
 				</DialogHeader>
 				<ScrollArea>
-					<div className="max-h-[75vh]">
-						{/* {data ? (
-              <>
-                {data?.map((log: any) => (
-                  <p key={log.id}>{log.user_name}</p>
-                ))}
-              </>
-            ) : (
-              <> */}
-
-						<DialogDescription className="flex flex-col items-center justify-center text-center gap-7 min-h-[280px] max-w-xs mx-auto">
-							<FiActivity className="text-5xl mx-auto text-gray-400" />
-							No recent activity detected. Please make sure the employee is
-							active.
-						</DialogDescription>
-
-						{/* </>
-            )} */}
+					<div className="max-h-[300px]">
+						{isLoading ? (
+							<>
+								<div className="space-y-4 mt-4">
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+								</div>
+							</>
+						) : data ? (
+							<div className="space-y-3">
+								{data
+									?.sort((a: any, b: any) => (a.id < b.id ? 1 : -1))
+									?.map((log: any) => (
+										<p
+											key={log.id}
+											className="text-sm font-semibold"
+										>
+											<span className="text-muted-foreground text-xs font-semibold">
+												Session ID: {log.id}
+											</span>
+											<br />
+											<b className="text-primary font-semibold underline">
+												Login Time:
+											</b>{" "}
+											{moment(log.login_time).format("lll")}
+										</p>
+									))}
+							</div>
+						) : (
+							<>
+								<DialogDescription className="flex flex-col items-center justify-center text-center gap-7 min-h-[280px] max-w-xs mx-auto">
+									<FiActivity className="text-5xl mx-auto text-gray-400" />
+									No recent activity detected. Please make sure the employee is
+									active.
+								</DialogDescription>
+							</>
+						)}
 					</div>
 				</ScrollArea>
 			</DialogContent>
