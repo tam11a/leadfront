aebee82ea37d0ab0f7d4c62f7e18b7dc9050b86c
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-28 09:06:05 +0600

    added: property and media pagination

--- src/app/(private)/medias/table.tsx
+++ src/app/(private)/medias/table.tsx
@@ -2,346 +2,369 @@
 
 import * as React from "react";
 import {
-  CaretSortIcon,
-  DotsHorizontalIcon,
-  MixerHorizontalIcon,
+	CaretSortIcon,
+	DotsHorizontalIcon,
+	MixerHorizontalIcon,
 } from "@radix-ui/react-icons";
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
 } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { TbUserEdit } from "react-icons/tb";
 import Link from "next/link";
 import { useGetMedias } from "@/lib/actions/media/get-medias";
 import { UpdateMedia } from "./update-media";
+import { parseAsInteger, useQueryState } from "nuqs";
 
 export interface Media {
-  id: number;
-  first_name: string;
-  last_name: string;
-  gender: string;
-  status: string;
-  priority: string;
-  source: string;
-  email: string;
-  phone: string;
-  dob: string;
-  bank_name?: string;
-  bank_branch?: string;
-  bank_account_number?: number;
-  bank_routing_number?: number;
-  address: string;
-  address2?: string;
-  zip_code: number;
-  nid: number;
-  is_active: boolean;
-  followup: string;
-  created_at: string;
-  updated_at: string;
-  deleted_at: string;
-  assigned_employee_id?: number;
-  media_id?: number;
-  project_id?: number;
+	id: number;
+	first_name: string;
+	last_name: string;
+	gender: string;
+	status: string;
+	priority: string;
+	source: string;
+	email: string;
+	phone: string;
+	dob: string;
+	bank_name?: string;
+	bank_branch?: string;
+	bank_account_number?: number;
+	bank_routing_number?: number;
+	address: string;
+	address2?: string;
+	zip_code: number;
+	nid: number;
+	is_active: boolean;
+	followup: string;
+	created_at: string;
+	updated_at: string;
+	deleted_at: string;
+	assigned_employee_id?: number;
+	media_id?: number;
+	project_id?: number;
 }
 
 export const columns: ColumnDef<Media>[] = [
-  {
-    accessorKey: "id",
-    header: () => {
-      return <div className="mx-4">ID</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("id")}</div>,
-  },
-  {
-    id: "full_name",
-    accessorKey: "full_name",
-    header: () => {
-      return <div className="mx-4">Full Name</div>;
-    },
-    cell: ({ row }) => (
-      <Link href={`/medias/${row.original.id}`} className="mx-4">
-        <Button variant={"link"}>
-          {row.original.first_name} {row.original.last_name}
-        </Button>
-      </Link>
-    ),
-  },
-  {
-    accessorKey: "phone",
-    header: () => {
-      return <div className="mx-4">Phone</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("phone")}</div>,
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => (
-      <>
-        <div className="lowercase">{row.getValue("email")}</div>
-      </>
-    ),
-  },
-  {
-    accessorKey: "gender",
-    header: () => {
-      return <div className="mx-4">Gender</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("gender")}</div>,
-  },
-  {
-    accessorKey: "dob",
-    header: () => {
-      return <div className="mx-4">Date of Birth</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("dob")}</div>,
-  },
-  {
-    accessorKey: "created_at",
-    header: () => {
-      return <div className="mx-4">Created At</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("created_at")}</div>,
-  },
+	{
+		accessorKey: "id",
+		header: () => {
+			return <div className="mx-4">ID</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("id")}</div>,
+	},
+	{
+		id: "full_name",
+		accessorKey: "full_name",
+		header: () => {
+			return <div className="mx-4">Full Name</div>;
+		},
+		cell: ({ row }) => (
+			<Link
+				href={`/medias/${row.original.id}`}
+				className="mx-4"
+			>
+				<Button variant={"link"}>
+					{row.original.first_name} {row.original.last_name}
+				</Button>
+			</Link>
+		),
+	},
+	{
+		accessorKey: "phone",
+		header: () => {
+			return <div className="mx-4">Phone</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("phone")}</div>,
+	},
+	{
+		accessorKey: "email",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Email
+					<CaretSortIcon className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => (
+			<>
+				<div className="lowercase">{row.getValue("email")}</div>
+			</>
+		),
+	},
+	{
+		accessorKey: "gender",
+		header: () => {
+			return <div className="mx-4">Gender</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("gender")}</div>,
+	},
+	{
+		accessorKey: "dob",
+		header: () => {
+			return <div className="mx-4">Date of Birth</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("dob")}</div>,
+	},
+	{
+		accessorKey: "created_at",
+		header: () => {
+			return <div className="mx-4">Created At</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("created_at")}</div>,
+	},
 
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const media = row.original;
-      return (
-        <>
-          <UpdateMedia mediaId={media.id}>
-            <Button size={"icon"} variant={"ghost"}>
-              <TbUserEdit />
-            </Button>
-          </UpdateMedia>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <DotsHorizontalIcon className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() =>
-                  navigator.clipboard.writeText(
-                    `ID: ${media.id?.toString()}\nFirst Name: ${
-                      media?.first_name
-                    }\nLast Name: ${media?.last_name}\nPhone: ${
-                      media.phone
-                    }\nEmail: ${media.email}`
-                  )
-                }
-              >
-                Copy Information
-              </DropdownMenuItem>
+	{
+		id: "actions",
+		enableHiding: false,
+		cell: ({ row }) => {
+			const media = row.original;
+			return (
+				<>
+					<UpdateMedia mediaId={media.id}>
+						<Button
+							size={"icon"}
+							variant={"ghost"}
+						>
+							<TbUserEdit />
+						</Button>
+					</UpdateMedia>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								variant="ghost"
+								className="h-8 w-8 p-0"
+							>
+								<span className="sr-only">Open menu</span>
+								<DotsHorizontalIcon className="h-4 w-4" />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							<DropdownMenuLabel>Actions</DropdownMenuLabel>
+							<DropdownMenuItem
+								onClick={() =>
+									navigator.clipboard.writeText(
+										`ID: ${media.id?.toString()}\nFirst Name: ${
+											media?.first_name
+										}\nLast Name: ${media?.last_name}\nPhone: ${
+											media.phone
+										}\nEmail: ${media.email}`
+									)
+								}
+							>
+								Copy Information
+							</DropdownMenuItem>
 
-              <DropdownMenuSeparator />
-              <Link href={`/medias/${media.id}`}>
-                <DropdownMenuItem>View profile</DropdownMenuItem>
-              </Link>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </>
-      );
-    },
-  },
+							<DropdownMenuSeparator />
+							<Link href={`/medias/${media.id}`}>
+								<DropdownMenuItem>View profile</DropdownMenuItem>
+							</Link>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</>
+			);
+		},
+	},
 ];
 
 export default function MediaTable() {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = React.useState({});
+	const [sorting, setSorting] = React.useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[]
+	);
+	const [columnVisibility, setColumnVisibility] =
+		React.useState<VisibilityState>({});
+	const [rowSelection, setRowSelection] = React.useState({});
 
-  const [search, setSearch] = React.useState<string>("");
-  const { data } = useGetMedias({ search });
+	const [search, setSearch] = useQueryState("search", {
+		defaultValue: "",
+		clearOnDefault: true,
+	});
 
-  const table = useReactTable({
-    data: React.useMemo(() => data?.data?.results || [], [data]),
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  });
+	const [page, setPage] = useQueryState(
+		"page",
+		parseAsInteger.withDefault(1).withOptions({
+			clearOnDefault: true,
+		})
+	);
 
-  return (
-    <div className="w-full max-w-[85vw] lg:max-w-[70vw] mx-auto relatives">
-      <div className="flex items-center flex-row gap-2 py-4">
-        <Input
-          placeholder="Search..."
-          value={search}
-          onChange={(event) => {
-            setSearch(event.target.value);
-          }}
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              View <MixerHorizontalIcon className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {table
-              .getAllColumns()
-              .filter(
-                (column) =>
-                  typeof column.accessorFn !== "undefined" &&
-                  column.getCanHide()
-              )
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+	const { data } = useGetMedias({ search, page });
 
-      <ScrollArea className="relative max-w-full whitespace-nowrap rounded-md border">
-        <Table className="w-full">
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+	const table = useReactTable({
+		data: React.useMemo(() => data?.data?.results || [], [data]),
+		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection,
+		},
+	});
 
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full max-w-[85vw] lg:max-w-[70vw] mx-auto relatives">
+			<div className="flex items-center flex-row gap-2 py-4">
+				<Input
+					placeholder="Search..."
+					value={search}
+					onChange={(event) => {
+						setSearch(event.target.value);
+					}}
+					className="max-w-sm"
+				/>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button
+							variant="outline"
+							className="ml-auto"
+						>
+							View <MixerHorizontalIcon className="ml-2 h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						{table
+							.getAllColumns()
+							.filter(
+								(column) =>
+									typeof column.accessorFn !== "undefined" &&
+									column.getCanHide()
+							)
+							.map((column) => {
+								return (
+									<DropdownMenuCheckboxItem
+										key={column.id}
+										className="capitalize"
+										checked={column.getIsVisible()}
+										onCheckedChange={(value) =>
+											column.toggleVisibility(!!value)
+										}
+									>
+										{column.id}
+									</DropdownMenuCheckboxItem>
+								);
+							})}
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+
+			<ScrollArea className="relative max-w-full whitespace-nowrap rounded-md border">
+				<Table className="w-full">
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+												  )}
+										</TableHead>
+									);
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && "selected"}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+				<ScrollBar orientation="horizontal" />
+			</ScrollArea>
+
+			<div className="flex items-center justify-end space-x-2 py-4">
+				<div className="flex-1 text-sm text-muted-foreground">
+					{page} of {Math.ceil((data?.data?.count || 1) / 8)} page(s).
+				</div>
+				<div className="space-x-2">
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => setPage((p) => p - 1)}
+						disabled={!data?.data?.previous}
+					>
+						Previous
+					</Button>
+					<Button
+						variant="outline"
+						size="sm"
+						onClick={() => setPage((p) => p + 1)}
+						disabled={!data?.data?.next}
+					>
+						Next
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
