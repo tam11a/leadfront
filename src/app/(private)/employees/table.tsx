04f275d23b1251703b553f0d80145cba811d20ca
tam11a <ibrahimsadiktamim@gmail.com>
2024-05-12 19:44:04 +0600

    removed: employee table frontend pagination

--- src/app/(private)/employees/table.tsx
+++ src/app/(private)/employees/table.tsx
@@ -2,41 +2,40 @@
 
 import * as React from "react";
 import {
-  CaretSortIcon,
-  DotsHorizontalIcon,
-  MixerHorizontalIcon,
+	CaretSortIcon,
+	DotsHorizontalIcon,
+	MixerHorizontalIcon,
 } from "@radix-ui/react-icons";
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getSortedRowModel,
+	useReactTable,
 } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { useEmployees } from "@/lib/actions/employees/users";
@@ -49,307 +48,315 @@ import { CreateAccessDialog } from "./create-access";
 import { parseAsInteger, useQueryState } from "nuqs";
 
 export interface Employee {
-  id: number;
-  first_name: string;
-  last_name: string;
-  employee_uid: string;
-  gender: string;
-  email: string;
-  phone: string;
-  dob: string;
-  work_hour: number;
-  salary: number;
-  bank_name?: string;
-  bank_branch?: string;
-  bank_account_number?: number;
-  bank_routing_number?: number;
-  address: string;
-  address2?: null;
-  zip_code: number;
-  nid: number;
-  tin?: number;
-  is_active: boolean;
-  created_at: string;
-  updated_at: string;
-  deleted_at: string;
-  user_id?: number;
+	id: number;
+	first_name: string;
+	last_name: string;
+	employee_uid: string;
+	gender: string;
+	email: string;
+	phone: string;
+	dob: string;
+	work_hour: number;
+	salary: number;
+	bank_name?: string;
+	bank_branch?: string;
+	bank_account_number?: number;
+	bank_routing_number?: number;
+	address: string;
+	address2?: null;
+	zip_code: number;
+	nid: number;
+	tin?: number;
+	is_active: boolean;
+	created_at: string;
+	updated_at: string;
+	deleted_at: string;
+	user_id?: number;
 }
 
 export const columns: ColumnDef<Employee>[] = [
-  // {
-  // 	accessorKey: "id",
-  // 	header: () => {
-  // 		return <div className="mx-4">Id</div>;
-  // 	},
-  // 	cell: ({ row }) => <div className="mx-4">{row.getValue("id")}</div>,
-  // },
-  {
-    accessorKey: "employee_uid",
-    header: () => {
-      return <div className="mx-4">Employee UID</div>;
-    },
-    cell: ({ row }) => (
-      <div className="mx-4">{row.getValue("employee_uid")}</div>
-    ),
-  },
-  {
-    id: "full_name",
-    accessorKey: "full_name",
-    header: () => {
-      return <div className="mx-4">Full Name</div>;
-    },
-    cell: ({ row }) => (
-      <div className="mx-4">
-        {row.original.first_name} {row.original.last_name}{" "}
-        {!row.original.user_id && (
-          <>
-            <CreateAccessDialog
-              email={row.original.email}
-              employeeId={row.original.id}
-            />
-          </>
-        )}
-      </div>
-    ),
-  },
-  {
-    accessorKey: "phone",
-    header: () => {
-      return <div className="mx-4">Phone</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("phone")}</div>,
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => (
-      <>
-        <div className="lowercase">{row.getValue("email")}</div>
-      </>
-    ),
-  },
-  {
-    accessorKey: "created_at",
-    header: () => {
-      return <div className="mx-4">Created At</div>;
-    },
-    cell: ({ row }) => <div className="mx-4">{row.getValue("created_at")}</div>,
-  },
-  {
-    accessorKey: "is_active",
-    header: () => {
-      return <div className="mx-4">Status</div>;
-    },
-    cell: ({ row }) => {
-      const isActive = row.getValue("is_active");
-      return (
-        <div className="mx-4">
-          <Badge variant={isActive ? "secondary" : "destructive"}>
-            {isActive ? "Active" : "Inactive"}
-          </Badge>
-        </div>
-      );
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const employee = row.original;
-      return (
-        <>
-          <UpdateEmployee employeeId={employee.id}>
-            <Button size={"icon"} variant={"ghost"}>
-              <TbUserEdit />
-            </Button>
-          </UpdateEmployee>
-          <LogDialog employeeId={employee.id}>
-            <Button
-              size={"icon"}
-              variant={"ghost"}
-              disabled={!employee.user_id}
-            >
-              <FiActivity />
-            </Button>
-          </LogDialog>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <DotsHorizontalIcon className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() =>
-                  navigator.clipboard.writeText(
-                    `ID: ${employee.id?.toString()}\nFirst Name: ${
-                      employee?.first_name
-                    }\nLast Name: ${employee?.last_name}\nPhone: ${
-                      employee.phone
-                    }\nEmail: ${employee.email}`
-                  )
-                }
-              >
-                Copy Information
-              </DropdownMenuItem>
+	// {
+	// 	accessorKey: "id",
+	// 	header: () => {
+	// 		return <div className="mx-4">Id</div>;
+	// 	},
+	// 	cell: ({ row }) => <div className="mx-4">{row.getValue("id")}</div>,
+	// },
+	{
+		accessorKey: "employee_uid",
+		header: () => {
+			return <div className="mx-4">Employee UID</div>;
+		},
+		cell: ({ row }) => (
+			<div className="mx-4">{row.getValue("employee_uid")}</div>
+		),
+	},
+	{
+		id: "full_name",
+		accessorKey: "full_name",
+		header: () => {
+			return <div className="mx-4">Full Name</div>;
+		},
+		cell: ({ row }) => (
+			<div className="mx-4">
+				{row.original.first_name} {row.original.last_name}{" "}
+				{!row.original.user_id && (
+					<>
+						<CreateAccessDialog
+							email={row.original.email}
+							employeeId={row.original.id}
+						/>
+					</>
+				)}
+			</div>
+		),
+	},
+	{
+		accessorKey: "phone",
+		header: () => {
+			return <div className="mx-4">Phone</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("phone")}</div>,
+	},
+	{
+		accessorKey: "email",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Email
+					<CaretSortIcon className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => (
+			<>
+				<div className="lowercase">{row.getValue("email")}</div>
+			</>
+		),
+	},
+	{
+		accessorKey: "created_at",
+		header: () => {
+			return <div className="mx-4">Created At</div>;
+		},
+		cell: ({ row }) => <div className="mx-4">{row.getValue("created_at")}</div>,
+	},
+	{
+		accessorKey: "is_active",
+		header: () => {
+			return <div className="mx-4">Status</div>;
+		},
+		cell: ({ row }) => {
+			const isActive = row.getValue("is_active");
+			return (
+				<div className="mx-4">
+					<Badge variant={isActive ? "secondary" : "destructive"}>
+						{isActive ? "Active" : "Inactive"}
+					</Badge>
+				</div>
+			);
+		},
+	},
+	{
+		id: "actions",
+		enableHiding: false,
+		cell: ({ row }) => {
+			const employee = row.original;
+			return (
+				<>
+					<UpdateEmployee employeeId={employee.id}>
+						<Button
+							size={"icon"}
+							variant={"ghost"}
+						>
+							<TbUserEdit />
+						</Button>
+					</UpdateEmployee>
+					<LogDialog employeeId={employee.id}>
+						<Button
+							size={"icon"}
+							variant={"ghost"}
+							disabled={!employee.user_id}
+						>
+							<FiActivity />
+						</Button>
+					</LogDialog>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								variant="ghost"
+								className="h-8 w-8 p-0"
+							>
+								<span className="sr-only">Open menu</span>
+								<DotsHorizontalIcon className="h-4 w-4" />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							<DropdownMenuLabel>Actions</DropdownMenuLabel>
+							<DropdownMenuItem
+								onClick={() =>
+									navigator.clipboard.writeText(
+										`ID: ${employee.id?.toString()}\nFirst Name: ${
+											employee?.first_name
+										}\nLast Name: ${employee?.last_name}\nPhone: ${
+											employee.phone
+										}\nEmail: ${employee.email}`
+									)
+								}
+							>
+								Copy Information
+							</DropdownMenuItem>
 
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>View profile</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </>
-      );
-    },
-  },
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>View profile</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</>
+			);
+		},
+	},
 ];
 
 export default function EmployeeTable() {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
+	const [sorting, setSorting] = React.useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[]
+	);
+	const [columnVisibility, setColumnVisibility] =
+		React.useState<VisibilityState>({});
 
-  const [page, setPage] = useQueryState(
-    "page",
-    parseAsInteger.withDefault(1).withOptions({
-      clearOnDefault: true,
-    })
-  );
+	const [page, setPage] = useQueryState(
+		"page",
+		parseAsInteger.withDefault(1).withOptions({
+			clearOnDefault: true,
+		})
+	);
 
-  const { data } = useEmployees({ page });
+	const { data } = useEmployees({ page });
 
-  const table = useReactTable({
-    data: React.useMemo(() => data?.data || [], [data]),
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-    },
-  });
+	const table = useReactTable({
+		data: React.useMemo(() => data?.data || [], [data]),
+		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		onColumnVisibilityChange: setColumnVisibility,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+		},
+	});
 
-  return (
-    <div className="w-full max-w-[85vw] lg:max-w-[70vw] mx-auto relative">
-      <div className="flex items-center flex-row gap-2 py-4">
-        <Input
-          placeholder="Filter email..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto flex">
-              View
-              <MixerHorizontalIcon className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {table
-              .getAllColumns()
-              .filter(
-                (column) =>
-                  typeof column.accessorFn !== "undefined" &&
-                  column.getCanHide()
-              )
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id.split("_").join(" ")}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+	return (
+		<div className="w-full max-w-[85vw] lg:max-w-[70vw] mx-auto relative">
+			<div className="flex items-center flex-row gap-2 py-4">
+				<Input
+					placeholder="Filter email..."
+					value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+					onChange={(event) =>
+						table.getColumn("email")?.setFilterValue(event.target.value)
+					}
+					className="max-w-sm"
+				/>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button
+							variant="outline"
+							className="ml-auto flex"
+						>
+							View
+							<MixerHorizontalIcon className="ml-2 h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						{table
+							.getAllColumns()
+							.filter(
+								(column) =>
+									typeof column.accessorFn !== "undefined" &&
+									column.getCanHide()
+							)
+							.map((column) => {
+								return (
+									<DropdownMenuCheckboxItem
+										key={column.id}
+										className="capitalize"
+										checked={column.getIsVisible()}
+										onCheckedChange={(value) =>
+											column.toggleVisibility(!!value)
+										}
+									>
+										{column.id.split("_").join(" ")}
+									</DropdownMenuCheckboxItem>
+								);
+							})}
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 
-      <ScrollArea className="relative max-w-full whitespace-nowrap rounded-md border">
-        <Table className="w-full">
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+			<ScrollArea className="relative max-w-full whitespace-nowrap rounded-md border">
+				<Table className="w-full">
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+												  )}
+										</TableHead>
+									);
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && "selected"}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+				<ScrollBar orientation="horizontal" />
+			</ScrollArea>
 
-      {/* PAGINATION */}
+			{/* PAGINATION */}
 
-      {/* <div className="flex items-center justify-end space-x-2 py-4">
+			{/* <div className="flex items-center justify-end space-x-2 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
           {page} of {Math.ceil((data?.data?.count || 1) / 8)} page(s).
         </div>
@@ -372,6 +379,6 @@ export default function EmployeeTable() {
           </Button>
         </div>
       </div> */}
-    </div>
-  );
+		</div>
+	);
 }
