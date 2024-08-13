d81df3c12adc08aa2c1972db5386f8ceb40858da
abdus satter <asatter833@gmail.com>
2024-08-13 23:21:29 +0600

    new branch init

--- src/app/(private)/customers/[id]/logs/create-log.tsx
+++ src/app/(private)/customers/[id]/logs/create-log.tsx
@@ -1,33 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-	DialogDescription,
-	Dialog,
-	DialogContent,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  DialogDescription,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { useGetCustomerById } from "@/lib/actions/customers/get-by-id";
@@ -40,47 +40,47 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const CreateCustomerMessageSchema = z.object({
-	note: z.string().optional(),
-	followup: z.any().optional(),
-	status: z.any().optional(),
+  note: z.string().optional(),
+  followup: z.any().optional(),
+  status: z.any().optional(),
 });
 
 type CreateCustomerMessageFormValues = z.infer<
-	typeof CreateCustomerMessageSchema
+  typeof CreateCustomerMessageSchema
 >;
 
 export function CreateLog({ id }: Readonly<{ id: number }>) {
-	// const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
-	const { data: customer, isLoading } = useGetCustomerById(id);
-	const form = useForm<CreateCustomerMessageFormValues>({
-		resolver: zodResolver(CreateCustomerMessageSchema),
-		defaultValues: {
-			note: "",
-			followup: undefined,
-			status: undefined,
-		},
-		mode: "onChange",
-	});
+  const { data: customer, isLoading } = useGetCustomerById(id);
+  const form = useForm<CreateCustomerMessageFormValues>({
+    resolver: zodResolver(CreateCustomerMessageSchema),
+    defaultValues: {
+      note: "",
+      followup: undefined,
+      status: undefined,
+    },
+    mode: "onChange",
+  });
 
-	useEffect(() => {
-		if (isLoading || !customer) return;
-		form.reset({
-			note: "",
-			status: customer.data.status,
-			followup: customer.data.followup,
-		});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [customer]);
+  useEffect(() => {
+    if (isLoading || !customer) return;
+    form.reset({
+      note: "",
+    });
+    //status: customer.data.status,
+    //followup: customer.data.followup,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [customer]);
 
-	console.log(form.formState.dirtyFields);
+  console.log(form.formState.dirtyFields);
 
-	const onSubmit = async (values: CreateCustomerMessageFormValues) => {
-		console.log(values);
-	};
-	return (
-		<>
-			{/* <Dialog open={open} onOpenChange={(o) => setOpen(o)}>
+  const onSubmit = async (values: CreateCustomerMessageFormValues) => {
+    console.log(values);
+  };
+  return (
+    <>
+      {/* <Dialog open={open} onOpenChange={(o) => setOpen(o)}>
       <DialogTrigger asChild>
         <Button onClick={() => setOpen(true)}>Create Log</Button>
       </DialogTrigger>
@@ -89,121 +89,115 @@ export function CreateLog({ id }: Readonly<{ id: number }>) {
           <DialogTitle>Add Log</DialogTitle>
           <DialogDescription>Create a custtomer log.</DialogDescription>
         </DialogHeader> */}
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					onReset={() => {
-						if (isLoading || !customer) return;
-						form.reset({
-							note: "",
-							status: customer.data.status,
-							followup: customer.data.followup,
-						});
-					}}
-				>
-					<FormField
-						control={form.control}
-						name="status"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Status</FormLabel>
-								<Select
-									onValueChange={(v) => v && field.onChange(v)}
-									value={field.value}
-								>
-									<FormControl>
-										<SelectTrigger>
-											<SelectValue placeholder="Select a status" />
-										</SelectTrigger>
-									</FormControl>
-									<SelectContent>
-										<SelectItem value="Raw">Raw</SelectItem>
-										<SelectItem value="Prospect">Prospect</SelectItem>
-										<SelectItem value="High Prospect">High Prospect</SelectItem>
-										<SelectItem value="Priority">Priority</SelectItem>
-										<SelectItem value="Booked">Booked</SelectItem>
-										<SelectItem value="Sold">Sold</SelectItem>
-										<SelectItem value="Closed">Closed</SelectItem>
-										<SelectItem value="Junk">Junk</SelectItem>
-									</SelectContent>
-								</Select>
-								<FormDescription></FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="followup"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Follow Up</FormLabel>
-								<FormControl>
-									<Popover>
-										<PopoverTrigger asChild>
-											<Button
-												variant={"outline"}
-												className={cn(
-													"w-full justify-start text-left font-normal",
-													!field.value && "text-muted-foreground"
-												)}
-											>
-												<CalendarIcon className="mr-2 h-4 w-4" />
-												{field.value ? (
-													format(field.value, "PPP")
-												) : (
-													<span>Pick a date</span>
-												)}
-											</Button>
-										</PopoverTrigger>
-										<PopoverContent className="w-auto p-0">
-											<Calendar
-												mode="single"
-												fromDate={new Date()}
-												selected={new Date(field.value)}
-												onSelect={(e: any) =>
-													field.onChange(format(e as Date, "yyyy-MM-dd"))
-												}
-												initialFocus={false}
-											/>
-										</PopoverContent>
-									</Popover>
-								</FormControl>
-								<FormDescription></FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="note"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Note</FormLabel>
-								<FormControl>
-									<Textarea
-										placeholder="Aa.."
-										{...field}
-									/>
-								</FormControl>
-								<FormDescription></FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<div className="flex flex-row flex-wrap gap-2">
-						<Button type="submit">Create Log</Button>
-						<Button
-							type="reset"
-							variant={"secondary"}
-						>
-							Reset
-						</Button>
-					</div>
-				</form>
-			</Form>
-			{/* </DialogContent>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          onReset={() => {
+            if (isLoading || !customer) return;
+            form.reset({
+              note: "",
+              status: customer.data.status,
+              followup: customer.data.followup,
+            });
+          }}
+        >
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  onValueChange={(v) => v && field.onChange(v)}
+                  value={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a status" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Raw">Raw</SelectItem>
+                    <SelectItem value="Prospect">Prospect</SelectItem>
+                    <SelectItem value="High Prospect">High Prospect</SelectItem>
+                    <SelectItem value="Priority">Priority</SelectItem>
+                    <SelectItem value="Booked">Booked</SelectItem>
+                    <SelectItem value="Sold">Sold</SelectItem>
+                    <SelectItem value="Closed">Closed</SelectItem>
+                    <SelectItem value="Junk">Junk</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="followup"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Follow Up</FormLabel>
+                <FormControl>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-full justify-start text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        fromDate={new Date()}
+                        selected={new Date(field.value)}
+                        onSelect={(e: any) =>
+                          field.onChange(format(e as Date, "yyyy-MM-dd"))
+                        }
+                        initialFocus={false}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="note"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Note</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Aa.." {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-row flex-wrap gap-2">
+            <Button type="submit">Create Log</Button>
+            <Button type="reset" variant={"secondary"}>
+              Reset
+            </Button>
+          </div>
+        </form>
+      </Form>
+      {/* </DialogContent>
     </Dialog> */}
-		</>
-	);
+    </>
+  );
 }
