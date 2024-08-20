162fae5c683281ff780dd3b068d921e9907a23fb
asatter833 <asatter833@gmail.com>
2024-08-20 15:52:07 +0600

    logs sorted according to descending value

--- src/app/(private)/customers/[id]/logs/log-list.tsx
+++ src/app/(private)/customers/[id]/logs/log-list.tsx
@@ -60,45 +60,47 @@ export default function CustomerLogsPage({ id }: Readonly<{ id: number }>) {
   return (
     <div className="space-y-3 px-8 py-6 border-l h-full min-h-[400px] md:min-w-[300px]">
       <CreateLog id={id} />
-      {logData?.data?.map((item: any) => {
-        return (
-          <div key={item.id} className="my-4 flex flex-col gap-1">
-            {item?.type === 1 ? (
-              <>
-                <p className="font-semibold text-sm text-text-light"></p>
-              </>
-            ) : item?.type === 2 ? (
-              <></>
-            ) : item?.type === 3 ? (
-              <></>
-            ) : item?.type === 4 ? (
-              <></>
-            ) : item?.type === 5 ? (
-              <>
-                <div className="flex items-center gap-1">
-                  <p className="underline text-sm font-bold">{item?.name}</p>
-                  <p className="text-sm text-text-light">
-                    {item?.description}.
-                  </p>
-                </div>
+      {logData?.data
+        ?.sort((a: any, b: any) => b.id - a.id)
+        .map((item: any) => {
+          return (
+            <div key={item.id} className="my-4 flex flex-col gap-1">
+              {item?.type === 1 ? (
+                <>
+                  <p className="font-semibold text-sm text-text-light"></p>
+                </>
+              ) : item?.type === 2 ? (
+                <></>
+              ) : item?.type === 3 ? (
+                <></>
+              ) : item?.type === 4 ? (
+                <></>
+              ) : item?.type === 5 ? (
+                <>
+                  <div className="flex items-center gap-1">
+                    <p className="underline text-sm font-bold">{item?.name}</p>
+                    <p className="text-sm text-text-light">
+                      {item?.description}.
+                    </p>
+                  </div>
 
-                <p className="font-medium text-sm bg-slate-100 dark:bg-slate-900 text-text p-3 my-1 max-w-xs whitespace-pre-wrap rounded">
-                  {item?.note}
-                </p>
-              </>
-            ) : item?.type === "conversation" ? (
-              <div className="font-medium text-sm bg-slate-100 dark:bg-slate-900 text-text p-3 my-1 max-w-xs whitespace-pre-wrap rounded">
-                {item?.conversation}
-              </div>
-            ) : (
-              <></>
-            )}
-            <p className="font-semibold text-xs text-text-light">
-              {moment(item.created_at).format("llll")}
-            </p>
-          </div>
-        );
-      })}
+                  <p className="font-medium text-sm bg-slate-100 dark:bg-slate-900 text-text p-3 my-1 max-w-xs whitespace-pre-wrap rounded">
+                    {item?.note}
+                  </p>
+                </>
+              ) : item?.type === "conversation" ? (
+                <div className="font-medium text-sm bg-slate-100 dark:bg-slate-900 text-text p-3 my-1 max-w-xs whitespace-pre-wrap rounded">
+                  {item?.conversation}
+                </div>
+              ) : (
+                <></>
+              )}
+              <p className="font-semibold text-xs text-text-light">
+                {moment(item.created_at).format("ddd, MMM D, YYYY")}
+              </p>
+            </div>
+          );
+        })}
     </div>
   );
 }
