0236e6340f949c2d5d61a068297b5871cdc4e0ba
asatter833 <asatter833@gmail.com>
2024-08-28 01:15:11 +0600

    blah

--- src/app/(private)/properties/[id]/side-details.tsx
+++ src/app/(private)/properties/[id]/side-details.tsx
@@ -59,7 +59,7 @@ export default function PropertiesSideBar(details: {
 
       <CardTitle>Address</CardTitle>
       <CardDescription className="flex items-center space-x-2 text-wrap max-w-xs">
-        <IoLocationOutline className="text-primary text-base w-7 h-auto" />
+        <IoLocationOutline className="text-primary text-base " />
         <span>
           {[details.address, details.areaName].join(", ") || "No address added"}
         </span>
@@ -74,14 +74,20 @@ export default function PropertiesSideBar(details: {
       <CardTitle>Media</CardTitle>
       <CardDescription className="flex space-x-2 text-wrap max-w-xs items-center">
         <IoPersonOutline className="text-primary text-base" />
-        <Link
-          href={`/medias/${details?.media_id}`}
-          className="mx-4 text-primary underline-offset-4 hover:underline"
-        >
-          {[mediaData?.data?.first_name, mediaData?.data?.last_name].join(
-            " "
-          ) || "No media assigned"}
-        </Link>
+        {details?.media_id ? (
+          <>
+            <Link
+              href={`/medias/${details?.media_id}`}
+              className="mx-4 text-primary underline-offset-4 hover:underline"
+            >
+              {[mediaData?.data?.first_name, mediaData?.data?.last_name].join(
+                " "
+              ) || "No media assigned"}
+            </Link>
+          </>
+        ) : (
+          <p className="mx-4">No media assigned</p>
+        )}
       </CardDescription>
       {details.remarks && (
         <>
